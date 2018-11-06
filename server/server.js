046bfaaf84bb4ce017247662b1632c1a6b247571
EndarUliu <endar8@gmail.com>
2018-11-06 11:17:32 +0100

    socked added connect disconnect

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express =  require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+io.on('connection', (socket)=>{
+    console.log("New user connected!")
+
+    socket.on('disconnect', (socket)=>{
+        console.log("Server connection lost!")
+    });
+});
+
+
+
+
+server.listen(port, ()=>{
     console.log(`Server up on ${port}`);
 });
 
